cd6a14eb7b2ef9aee6de929222c1372e7cd3a370
Feihong Hsu <feihong.hsu@gmail.com>
2018-04-24 13:54:52 -0500

    Show multiple characters instead of just one

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,7 +1,10 @@
-import 'package:angular/angular.dart';
 import 'dart:math';
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
 
 final rand = new Random();
+const initialValue = 3;
 
 String getRandomHanzi() {
   const start = 0x4e00, end = 0x9fff;
@@ -10,12 +13,18 @@ String getRandomHanzi() {
   return new String.fromCharCode(n);
 }
 
+List<String> getRandomCharacters(int count) {
+  return new List<String>.generate(count, (i) => getRandomHanzi());
+}
+
 @Component(
   selector: 'cool-app',
   templateUrl: 'app_component.html',
   styleUrls: ['app_component.css'],
+  directives: [coreDirectives, formDirectives],
 )
 class AppComponent {
-  int count = 3;
-  String hanzi = getRandomHanzi();
+  String countStr = initialValue.toString();
+  int count = initialValue;
+  List<String> characters = getRandomCharacters(initialValue);
 }
